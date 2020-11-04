ac8a0790d21ceb07586eb16f4eb520afe57da6ea
Nguyen Thanh Nam <ntnam7399@gmail.com>
2020-11-04 13:48:54 +0700

    config database

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_10_14_083619) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
